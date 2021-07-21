e611e7e4896ae0fdd492549fdf0f75a85b34dfd0
Ronald Espinosa <ronaldesp@hotmail.com>
2021-07-21 18:55:43 -0500

    Initial Commit

---
+++ azf1.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace azfunc1
+{
+    public static class azf1
+    {
+        [FunctionName("azf1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("azf1 executed.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            
+            log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
+
+            return new OkObjectResult($"azf1 success!");
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Email { get; set; }
+        public decimal Price { get; set; }
+    }
+}
