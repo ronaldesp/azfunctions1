71fbc375b4e28c111fc8af451cba8e93b4a0f2e3
Ronald Espinosa <33752005+ronaldesp@users.noreply.github.com>
2021-07-23 23:01:17 -0500

    Add files via upload

--- PowerShellScripts/CreateAzFuncDNSUpdate.ps1
+++ PowerShellScripts/CreateAzFuncDNSUpdate.ps1
@@ -1,11 +1,21 @@
 [CmdletBinding()]
 param (
-    $ResourceGroupName='rg-OPS-functions-cace',
-    $LocationName='canadacentral',
-    $storageAccountName='devopsstoragefunctions2',
-    $appServicePlanName='plan-WinApp-Ops-Functions-cace',
-    $functionAppName='func-Ops-cf-dns-update02-cace'
+    $ResourceGroupName='rgfunctions',
+    $LocationName='eastus',
+    $storageAccountName='storageaccountazf',
+    $appServicePlanName='planazf',
+    $functionAppName='azf11'
 )
+
+    # $ResourceGroupName='rg-OPS-functions-cace',
+    # $LocationName='canadacentral',
+    # $storageAccountName='devopsstoragefunctions2',
+    # $appServicePlanName='plan-WinApp-Ops-Functions-cace',
+    # $functionAppName='func-Ops-cf-dns-update02-cace'
+
+az login
+az account set --subscription "a74522a1-9e20-48ba-8edd-afd9172ec929"
+az account list --output table
 
 $resourceGroup = $ResourceGroupName
 $location = $LocationName
@@ -19,7 +29,7 @@ Write-Host "storageAccount: " $storageAccount
 Write-Host "appServicePlan: " $appServicePlan
 Write-Host "functionApp: " $functionApp
 
-if (! (az group exists --name $resourceGroup))
+if (!(az group exists --name $resourceGroup))
 {
     Write-Host "Resource group does not exists. Creating..."
     # Create a Resource Group
@@ -55,19 +65,28 @@ if (!$planExists) {
 else {
     Write-Host $planCheck.Length
     Write-Host $planCheck
-    Write-Host "App Service Plan does not exists. Creating..."
+    Write-Host "App Service Plan already exists"
 }
 
+# $functionAppCheck = az functionapp function show -g $resourceGroup -n 'funcre' --function-name $functionApp | ConvertFrom-Json
+# $functionAppCheck = az functionapp function show -g $resourceGroup -n 'funcre' --function-name 'noexiste' | ConvertFrom-Json
+
+# $functionAppCheck = az functionapp list --query "[].{Name: name,Group: resourceGroup}" #"[?name=='$functionApp']" | ConvertFrom-Json
+# Write-Host $functionAppCheck
+
 $functionAppCheck = az functionapp list --query "[?name=='$functionApp']" | ConvertFrom-Json
+Write-Host $functionAppCheck
 $functionAppExists = $functionAppCheck.Length -gt 0
 if (!$functionAppExists) {
     Write-Host "Function App does not exists. Creating..."
+    Write-Host $functionAppCheck.Length
+    Write-Host $functionAppCheck
     # Create Azure Function App
-    az functionapp create -g $resourceGroup -p $appServicePlan -n $functionApp  -s $storageAccount --os-type Windows --functions-version 3
+    az functionapp create -g $resourceGroup -p $appServicePlan -n $functionApp  -s $storageAccount --os-type Windows --functions-version 3 # --debug
     Write-Host "Function App created"
 }
 else {
     Write-Host $functionAppCheck.Length
-    Write-Host $planCheck
+    Write-Host $functionAppCheck
 }
 
