261011bb6a44eb3a30b43fa3438c8c6676aca297
Ronald Espinosa <33752005+ronaldesp@users.noreply.github.com>
2021-07-23 21:28:28 -0500

    Delete directory

--- PowerShellScripts/RunScript.ps1
+++
@@ -1,3 +0,0 @@
-#powershell.exe .\PowerShellScripts\CreateAzFuncDNSUpdate.ps1 -ResourceGroupName 'rg-OPS-functions-cace1' -LocationName 'canadacentral1'
-#powershell.exe -Command .\PowerShellScripts\CreateAzFuncDNSUpdate.ps1 'rg-OPS-functions-cace1' 'canadacentral1'
-.\PowerShellScripts\CreateAzFuncDNSUpdate.ps1 -ResourceGroupName 'rg-OPS-functions-cace1' -LocationName 'canadacentral1'
